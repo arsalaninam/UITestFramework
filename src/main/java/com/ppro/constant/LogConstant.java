65d3c517f2358ac40057ba7f3de13be30a223fd1
arsalaninam <m.arsalaninam@gmail.com>
2020-08-13 18:19:28 +0200

    Add constants

---
+++ src/main/java/com/ppro/constant/LogConstant.java
@@ -0,0 +1,13 @@
+package com.ppro.constant;
+
+public interface LogConstant {
+
+    String TEST_TRANSACTION_SUCCEEDED_MESSAGE_IS_DISPLAYED_WITH_VALID_INPUT_COMBINATIONS =
+            "-- Test to verify that user is able to see Transaction Succeeded Message with valid payment input combinations --";
+
+    String TEST_TRANSACTION_FAILED_MESSAGE_IS_DISPLAYED_WITH_INVALID_INPUT_COMBINATIONS =
+            "-- Test to verify that user is able to see Transaction Failed Message with invalid payment input combinations --";
+
+    String TEST_TRANSACTION_FAILED_MESSAGE_IS_DISPLAYED_WITH_INVALID_AMOUNT_BUT_VALID_COUNTRY_AND_CURRENCY_COMBINATIONS =
+            "-- Test to verify that user is able to see Transaction Failed Message with invalid amount but valid country and currency input combinations --";
+}
