639ad3a648aca3d0204709581a9033a628750731
arsalaninam <m.arsalaninam@gmail.com>
2020-08-13 18:27:37 +0200

    Remove import

--- src/main/java/com/ppro/page/SimulationPaymentPage.java
+++ src/main/java/com/ppro/page/SimulationPaymentPage.java
@@ -5,7 +5,6 @@ import com.ppro.util.TestUtil;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 public class SimulationPaymentPage extends TestBase {
 
